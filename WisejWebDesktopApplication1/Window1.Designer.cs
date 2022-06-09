d4a3c6fbde32dd7b9f3a5d6feb91014edeba2b2c
Tung Ngo <ngo-thanh.tung@spc-technology.com>
2022-06-09 18:03:46 -0400

    Add project files.

---
+++ WisejWebDesktopApplication1/Window1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WisejWebDesktopApplication1
+{
+	partial class Window1
+{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Wisej Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.dataGridView1 = new Wisej.Web.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Dock = Wisej.Web.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(1673, 1010);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Window1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 19F);
+            this.AutoScaleMode = Wisej.Web.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1673, 1010);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Window1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+		}
+
+        #endregion
+
+        private Wisej.Web.DataGridView dataGridView1;
+    }
+}
+
---
+++ WisejWebDesktopApplication1/Window1.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using System.Collections.Generic;
+using Wisej.Web;
+
+namespace WisejWebDesktopApplication1
+{
+	public partial class Window1 : Form
+	{
+		public Window1()
+		{
+			InitializeComponent();
+            
+            this.dataGridView1.AutoGenerateColumns = true;
+            this.dataGridView1.DataSource = Person.GetSampleData();
+        }
+        
+        
+	}
+
+    class Person
+    {
+
+        public static List<Person> GetSampleData()
+        {
+            var ret = new List<Person>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                ret.Add(new Person() { Name = $"Person no {i}", PersonId = $"{i:000}" });
+            }
+
+            return ret;
+        }
+
+        public string PersonId { get; set; }
+        
+        public string Name { get; set; } 
+         
+
+    }
+}
